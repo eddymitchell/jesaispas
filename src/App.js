ee1869322a1af2a644794022e561c95bc715b3c8
eddymitchell <patrocuite@gmail.com>
2022-02-28 18:46:45 +0100

    changements2base

--- src/App.js
+++ src/App.js
@@ -5,13 +5,18 @@ import { Environment, OrbitControls } from '@react-three/drei'
 import Model from './Model'
 
 export default function App() {
+  const macamera = [25,25,70]
+  const popup =() => {
+    alert("voulez vous devenir riche?" + macamera)
+  };
   return (
     <div className="App">
-      <Canvas>
+      <h1 onClick={() => popup()} className='titre'>AMINTI</h1>
+      <Canvas camera={{fov: 30, position: macamera }}>
         <Suspense fallback={null}>
           <spotLight intensity= {2
 } position ={[15, 15, 15]} />
-        <OrbitControls />
+        <OrbitControls autoRotate/>
           <Model />
           <Environment preset="night" background />
         </Suspense>
